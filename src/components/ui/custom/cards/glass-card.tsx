d13400b42d81f3ab17789ec8c656cb7fc28fce4a
Kalu David <102557749+KaluDavid@users.noreply.github.com>
2025-09-10 06:18:00 +0100

    feat(sections): build Header, Home, About, and How Spot'D Works sections

---
+++ src/components/ui/custom/cards/glass-card.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../../card';
+import { CheckCheck } from 'lucide-react';
+import { Skeleton } from '../../skeleton';
+import clsx from 'clsx';
+
+export const Glass_Card = ({
+  title,
+  content,
+}: {
+  title: string;
+  content?: boolean;
+}) => {
+  return (
+    <Card
+      className={clsx(
+        'flex md:pt-2 pt-1 gap-1 px-1 md:px-[11px] *:p-0 md:space-y-2 border-none rounded-2xl md:rounded-3xl backdrop-blur-sm backdrop-red-500 bg-black/10 w-[140px] md:w-[302px]',
+        !content && 'md:pb-2 pb-1 rounded-full',
+        content && 'max-md:pb-2 max-md:!rounded-lg'
+      )}
+    >
+      <CardHeader className="flex gap-1.5 md:gap-3 justify-start items-center relative">
+        <span className="md:size-10.5 size-8 rounded-full bg-black/50 flex items-center justify-center">
+          <span className="md:size-7.5 bg-white rounded-full flex items-center justify-center ">
+            <CheckCheck strokeWidth={'3px'} size={'18px'} />
+          </span>
+        </span>
+        <CardTitle className="text-black font-medium  text-[8px] md:text-base">
+          {title}
+        </CardTitle>
+
+        {!content && (
+          <span className="rounded-full flex justify-center items-center text-sm bg-round-red md:size-6.5 size-3.5 text-white font-medium absolute right-0 p-2 -top-2 md:-top-4 max-md:text-[8px]">
+            1
+          </span>
+        )}
+      </CardHeader>
+      {content && (
+        <CardContent className="md:gap-2 gap-1 flex flex-col md:*:h-2.5 *:h-1 odd:w-full w-full *:bg-white/50">
+          <Skeleton />
+          <Skeleton className="w-[242px]" />
+          <Skeleton />
+          <Skeleton className="md:blur-[1px] w-[219px]" />
+        </CardContent>
+      )}
+    </Card>
+  );
+};
