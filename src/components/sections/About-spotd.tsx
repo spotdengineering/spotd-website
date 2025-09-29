9bb3a5f91c64419d0fb4c802be603965779ab498
PengMouse <tundejoseph294@gmail.com>
2025-09-29 16:36:26 +0100

    feat: fixes

--- src/components/sections/About-spotd.tsx
+++ src/components/sections/About-spotd.tsx
@@ -76,17 +76,16 @@ const About_Spotd = () => {
                       <p className="text-lg">{i?.sub}</p>
                     </motion.div>
                   ))}
-
-                  {/* phone */}
-                  <div className="">
-                    <img
-                      className="h-[350px] lg:h-[450px] w-[500px] object-contain"
-                      alt=""
-                      src="/iMax.png"
-                    />
-                  </div>
                 </div>
               </div>
+            </div>
+            {/* phone */}
+            <div className="">
+              <img
+                className="h-[350px] lg:h-[450px] w-[500px] object-contain"
+                alt=""
+                src="/iMax.png"
+              />
             </div>
           </div>
 
